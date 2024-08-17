59153bcad4d8d1cb08ffe3b53d3908880d8d4968
GowthamS2004 <99336040+GowthamS2004@users.noreply.github.com>
2024-08-17 15:55:10 +0530

    Time: 8 ms (61.04%), Space: 44.8 MB (52.45%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,24 +1,25 @@
 class Solution {
     public String shortestPalindrome(String s) {
         int n=s.length();
-        if(n==0){return s;}
-        if(isPal(s)){return s;}
+        
+        if( n==0 || isPal(s)){return s;}
         // find the length of largest pal
         // if there is no pal i=1 
         //else i=last index of largest pal
         int i=largest_pal_length(s);
-        String r=rev(s.substring(i));
+        String r=rev(s.substring(i));  // instead of adding one by one, added the reverse 
 
          // int i=1;
-        /*  while(!isPal(s))
-          {
+        /*  while(!isPal(s))    //adding one by one
+          {          
              s=s.charAt(i)+s;
              i+=2;
-          }*/
+          }*/         
 
         return r+s;
        
     }
+    //
     public boolean isPal(String str)
     {
         int s=0,e=str.length()-1;
@@ -33,17 +34,36 @@ class Solution {
         return true;
 
     }
+    //    
     public int largest_pal_length(String str)
     {
-        int n=str.length();
-        for(int i=n;i>=1;i--)
-        {
-            if(isPal(str.substring(0,i)))
-            {
-                return i;
+      String reversedStr = new StringBuilder(str).reverse().toString();
+    String combinedStr = str + "#" + reversedStr;
+    int n = str.length();
+    int[] lps = new int[combinedStr.length()];
+    
+    // Build LPS array for KMP algorithm
+    int len = 0;  // Length of the previous longest prefix suffix
+    int i = 1;    // Index in combinedStr
+    
+    while (i < combinedStr.length()) {
+        if (combinedStr.charAt(i) == combinedStr.charAt(len)) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
             }
         }
-        return 1;
+    }
+    
+    // The length of the longest palindromic prefix
+    return lps[combinedStr.length() - 1];
+     
     }
 
     //reverse
