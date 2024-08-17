bb126d6eb07934e655cfb573287ddbf6b262280c
GowthamS2004 <99336040+GowthamS2004@users.noreply.github.com>
2024-08-17 11:51:07 +0530

    Time: 29 ms (5.07%), Space: 45 MB (5.32%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,14 +1,38 @@
+import java.math.BigInteger;
 class Solution {
     public String largestNumber(int[] nums) {
         String s[] = new String[nums.length];
         for(int i=0;i<nums.length;i++){
             s[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(s,(a,b) -> (b+a).compareTo(a+b));
-        String ans="";
-        for(int i=0;i<nums.length;i++){
+       int n=nums.length;
+        for(int k=0;k<n;k++)
+        {
+            String max=s[0];  
+            for(int i=1;i<n-k;i++)
+            {
+                BigInteger a = new BigInteger(s[i - 1] + s[i]);
+                BigInteger b = new BigInteger(s[i] + s[i - 1]);
+                if(a.compareTo(b) < 0)
+                {
+                   max=s[i];
+                }
+                else
+                {
+                     String temp=s[i];
+                    s[i]=max;
+                    s[i-1]=temp;
+                    
+                }
+            }
+        }
+            
+       String ans="";
+        for(int i=n-1;i>=0;i--){
             ans+=s[i];
         }
-        return  ans;
+        return ans.charAt(0)=='0'?"0" : ans;
+       // Arrays.sort(s,(a,b) -> (b+a).compareTo(a+b)); 
+       /**/
     }
 }
