9d8e2fa902c86c2bdb8f3ff4f199682d67c1e247
GowthamS2004 <99336040+GowthamS2004@users.noreply.github.com>
2024-08-22 22:22:49 +0530

    Time: 0 ms (100%), Space: 40.8 MB (12.78%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,9 +1,13 @@
 class Solution {
     public int findComplement(int num) {
-        // Calculate the bit mask (a number with all bits set to 1 for the bit length of num)
-        int mask = (Integer.highestOneBit(num) << 1) - 1;
-        
-        // XOR the number with the mask to flip its bits
-        return num ^ mask;
+       int temp=num;
+       int t=0;
+       while(temp!=0)
+       {
+        temp>>=1;
+        t++;
+       }
+       int mask=(1<<t)-1;
+       return num^mask;
     }
 }
